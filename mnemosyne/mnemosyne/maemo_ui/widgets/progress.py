7d884d16809428ca8005109b56bc04b32f89d970
umax <maxusachev@gmail.com>
2010-02-13 12:01:57 +0200

    code cleaning

--- mnemosyne/mnemosyne/maemo_ui/widgets/progress.py
+++ mnemosyne/mnemosyne/maemo_ui/widgets/progress.py
@@ -24,70 +24,60 @@
 Hildon UI. Widget for progressbar window.
 """
 
-from mnemosyne.libmnemosyne.ui_components.dialogs import ProgressDialog
 import gtk
+from mnemosyne.libmnemosyne.ui_components.dialogs import ProgressDialog
+
 
 class MaemoProgressDlg(ProgressDialog):
-    """ Class for ProgressDlg in libmnemosyne. It using in factory"""
+    """Maemo Progress Dialog."""
 
     def __init__(self, component_manager):
         ProgressDialog.__init__(self, component_manager)
-        
         self.window = None
         self.fraction = 0.0
         try:
             import hildon
-            self.pbar = hildon.hildon_banner_show_progress(\
-                        self.main_widget().window, None, "")
+            self.pbar = hildon.hildon_banner_show_progress( \
+                self.main_widget().window, None, "")
             self.pbar.show()
         except ImportError:
-
             self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-
             self.window.set_title("ProgressBar")
             self.window.set_border_width(0)
-
             vbox = gtk.VBox(False, 5)
             vbox.set_border_width(10)
             self.window.add(vbox)
             vbox.show()
-     
-            # Create a centering alignment object
+            # create a centering alignment object
             align = gtk.Alignment(0.5, 0.5, 0, 0)
             vbox.pack_start(align, False, False, 5)
             align.show()
-
-            # Create the ProgressBar
+            # create the ProgressBar
             self.pbar = gtk.ProgressBar()
-
             align.add(self.pbar)
             self.pbar.show()
-
             separator = gtk.HSeparator()
             vbox.pack_start(separator, False, False, 0)
             separator.show()
-
             # rows, columns, homogeneous
             table = gtk.Table(2, 2, False)
             vbox.pack_start(table, False, True, 0)
             table.show()
-
-
 	
     def set_range(self, minimum, maximum):
-        """Calculate fraction for progressbar """
+        """Calculate fraction for progressbar."""
 
-        self.fraction = float(1.0/(maximum-minimum))
+        self.fraction = float(1.0 / (maximum - minimum))
         if self.window:
             self.window.show()
        
     def set_text(self, text):
-        """Set title on progress bar """
+        """Set title on progress bar."""
 
         self.pbar.set_text(text)
         
     def set_value(self, value):
-        """Set new value for progess bar """ 
+        """Set new value for progess bar.""" 
 
         self.pbar.set_fraction(value * self.fraction)
         if value * self.fraction > 0.999 :
@@ -95,8 +85,7 @@ class MaemoProgressDlg(ProgressDialog):
                 self.window.destroy()
             else:
                 self.pbar.destroy()
- 
-        #Pending gtk
+        # pending gtk
         while gtk.events_pending():
             gtk.main_iteration(False)
 
