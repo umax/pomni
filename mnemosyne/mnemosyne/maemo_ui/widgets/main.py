24da503762d87ec9967f0d0aa8bd3adf792301e4
plastun <plastun@lenovo.(none)>
2010-02-27 01:01:32 +0200

    adapted Dialogs to N900

--- mnemosyne/mnemosyne/maemo_ui/widgets/main.py
+++ mnemosyne/mnemosyne/maemo_ui/widgets/main.py
@@ -27,57 +27,35 @@ Hildon UI. Widgets for main.
 import gtk
 import hildon
 
+
 def create_question_dialog(window, text):
     """Create QuestionDialog UI."""
 
-    dialog = gtk.Dialog(parent=window, flags=gtk.DIALOG_MODAL|\
-            gtk.DIALOG_DESTROY_WITH_PARENT|gtk.DIALOG_NO_SEPARATOR)
-    dialog.set_decorated(False)
-    dialog.set_name('dialog')
-    button_yes = dialog.add_button('YES', gtk.RESPONSE_YES)
-    button_yes.set_size_request(120, 80)
-    button_yes.set_name('dialog_button')
-    button_no = dialog.add_button('NO', gtk.RESPONSE_REJECT)
-    button_no.set_size_request(120, 80)
-    button_no.set_name('dialog_button')
-    table = gtk.Table(rows=1, columns=1)
-    label = gtk.Label()
-    label.set_name('dialog_label')
-    label.set_justify(gtk.JUSTIFY_CENTER)
-    # make question text easy for user
     if 'Activate cards' in text:
         text = 'Delete this card and 1 related card?'
-    label.set_text(text)
-    table.attach(label, 0, 1, 0, 1, xpadding=30, ypadding=30)
-    table.show_all()
-    dialog.vbox.pack_start(table)
-    dialog.vbox.set_spacing(2)
-    dialog.action_area.set_layout(gtk.BUTTONBOX_CENTER)
+    label = gtk.Label(text)
+    dialog = hildon.Dialog()
+    dialog.set_title('')
+    dialog.vbox.add(label)
+    label.show()
+    dialog.add_button("Yes", gtk.RESPONSE_YES)
+    dialog.add_button("No", gtk.RESPONSE_NO)
     response = dialog.run()
     dialog.destroy()
     if response == gtk.RESPONSE_YES:
         return False
     return True
-
+                            
 
 def create_information_dialog(window, text):
     """Create InformationDialog UI."""
     
-    dialog = gtk.Dialog(parent=window, flags=gtk.DIALOG_MODAL|\
-        gtk.DIALOG_DESTROY_WITH_PARENT|gtk.DIALOG_NO_SEPARATOR)
-    dialog.set_decorated(False)
-    dialog.set_name('dialog')
-    button_ok = dialog.add_button('OK', gtk.RESPONSE_OK)
-    button_ok.set_size_request(120, 80)
-    button_ok.set_name('dialog_button')
-    table = gtk.Table(rows=1, columns=1)
+    dialog = hildon.Dialog()
+    dialog.set_title('')
     label = gtk.Label()
     label.set_justify(gtk.JUSTIFY_CENTER)
-    label.set_name('dialog_label')
-    label.set_text(text.replace('. ', '.   \n').replace(',', ',\n'))
-    table.attach(label, 0, 1, 0, 1, xpadding=30, ypadding=30)
-    table.show_all()
-    dialog.vbox.pack_start(table)
-    dialog.action_area.set_layout(gtk.BUTTONBOX_SPREAD)
+    label.set_text(text + '\n')
+    label.show()
+    dialog.vbox.pack_start(label)
     dialog.run()
     dialog.destroy()
