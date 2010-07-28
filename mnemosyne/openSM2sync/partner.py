bfa56ac56a687c33f6da7e1e157639834a068745
Peter Bienstman <Peter.Bienstman@UGent.be>
2010-07-28 10:19:21 +0200

    Improve error reporting.

---
+++ mnemosyne/openSM2sync/partner.py
@@ -0,0 +1,94 @@
+#
+# partner.py <Peter.Bienstman@UGent.be>
+#
+
+from utils import SyncError
+
+BUFFER_SIZE = 8192
+
+
+class Partner(object):
+
+    """Common code between Client and Server."""
+
+    def __init__(self, ui):
+        self.ui = ui
+
+    def stream_log_entries(self, log_entries, number_of_entries,
+                           progress_message):
+        # Send log entries across in a streaming manner.
+        # Normally, one would use "Transfer-Encoding: chunked" for that, but
+        # chunked requests are not supported by the WSGI 1.x standard.
+        # However, it seems we can get around sending a Content-Length header
+        # if the server knows when the datastream ends. We use the data format
+        # footer as a sentinel for that.
+        # As the first line in the stream, we send across the number of log
+        # entries, so that the other side can track progress.
+        # We also buffer the stream until we have sufficient data to send, in
+        # order to improve throughput.
+        # We also tried compression here, but for typical scenarios that is
+        # slightly slower on a WLAN and mobile phone.     
+        progress_dialog = self.ui.get_progress_dialog()
+        progress_dialog.set_range(0, number_of_entries)
+        progress_dialog.set_text(progress_message)
+        buffer = self.text_format.log_entries_header(number_of_entries)
+        count = 0
+        for log_entry in log_entries:
+            count += 1
+            progress_dialog.set_value(count)
+            buffer += self.text_format.repr_log_entry(log_entry)
+            if len(buffer) > BUFFER_SIZE:
+                yield buffer.encode("utf-8")
+                buffer = ""
+        buffer += self.text_format.log_entries_footer()
+        yield buffer.encode("utf-8")
+
+    def download_log_entries(self, stream, progress_message,
+                             callback, context):
+        element_loop = self.text_format.parse_log_entries(stream)
+        number_of_entries = element_loop.next()
+        if number_of_entries == 0:
+            return
+        progress_dialog = self.ui.get_progress_dialog()
+        progress_dialog.set_range(0, number_of_entries)
+        progress_dialog.set_text(progress_message)            
+        count = 0
+        for log_entry in element_loop:
+            callback(context, log_entry)
+            count += 1
+            progress_dialog.set_value(count)
+        progress_dialog.set_value(number_of_entries)
+        
+    def stream_binary_file(self, binary_file, file_size, progress_message):
+        progress_dialog = self.ui.get_progress_dialog()
+        progress_dialog.set_text(progress_message)
+        progress_dialog.set_range(0, file_size)        
+        buffer = str(file_size) + "\n" + binary_file.read(BUFFER_SIZE)
+        count = BUFFER_SIZE
+        while buffer:
+            progress_dialog.set_value(count)
+            yield buffer
+            buffer = binary_file.read(BUFFER_SIZE)
+            count += BUFFER_SIZE
+        progress_dialog.set_value(file_size)
+
+    def download_binary_file(self, filename, stream, progress_message):
+        downloaded_file = file(filename, "wb")
+        line = stream.readline()
+        if line == "CANCEL":
+            raise SyncError("Downloading binary file: server error.")
+        file_size = int(line)
+        progress_dialog = self.ui.get_progress_dialog()
+        progress_dialog.set_text(progress_message)
+        progress_dialog.set_range(0, file_size)
+        remaining = file_size
+        while remaining:
+            if remaining < BUFFER_SIZE:
+                downloaded_file.write(stream.read(remaining))
+                remaining = 0
+            else:
+                downloaded_file.write(stream.read(BUFFER_SIZE))
+                remaining -= BUFFER_SIZE
+            progress_dialog.set_value(file_size - remaining)
+        progress_dialog.set_value(file_size)
+        downloaded_file.close()
---
+++ mnemosyne/openSM2sync/utils.py
@@ -0,0 +1,41 @@
+#
+# utils.py - Peter Bienstman <Peter.Bienstman@UGent.be>
+
+import os
+import sys
+import math
+import traceback
+
+class SyncError(Exception):
+    pass
+
+def traceback_string():
+    
+    """Like traceback.print_exc(), but returns a string."""
+
+    type, value, tb = sys.exc_info()
+    body = "\nTraceback (innermost last):\n"
+    list = traceback.format_tb(tb, limit=None) + \
+           traceback.format_exception_only(type, value)
+    body = body + "%-20s %s" % ("".join(list[:-1]), list[-1])
+    return body
+
+def tar_file_size(basedir, relative_paths):
+
+    """Returns size in bytes of a tar file containing relative paths in a
+    given basedir.
+
+    """
+
+    # TODO: estimate only.
+
+    size = 0
+    if len(relative_paths) == 0:
+        return 0
+    for path in relative_paths:
+        raw_size = os.path.getsize(os.path.join(basedir, path))
+        # 512 byte header plus file content in blocks of 512 bytes.
+        size += 512 + int(math.ceil(raw_size / 512.0)) * 512
+    # 2 512 blocks zero to signal end + pad on 10240 byte boundary.
+    return int(math.ceil((size + 2*512) / 10240.)) * 10240
+    
