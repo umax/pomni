b8c9d8d750dbb34bd70db2fa2d2a70f7dc22b3b1
Max Usachev <maxusachev@gmail.com>
2011-02-03 00:00:43 +0200

    Finalize UDP work. (libmnemosyne)

--- mnemosyne/mnemosyne/UDP_server/server.py
+++ mnemosyne/mnemosyne/UDP_server/server.py
@@ -2,46 +2,62 @@
 # server.py <Peter.Bienstman@UGent.be>
 #
 
+import sys
+import select
 import SocketServer
 
 from mnemosyne.libmnemosyne import Mnemosyne
+from mnemosyne.libmnemosyne.utils import traceback_string
 
+
+class OutputCatcher:
+
+    def __init__(self, socket, client_address):
+        self.socket = socket
+        self.client_address = client_address
+
+    def write(self, data):
+        self.socket.sendto(data, self.client_address)
+        
 
 class MyHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        sys.stdout = socket
-        sys.stderr = socket
+        sys.stdout = sys.stderr = OutputCatcher(socket, self.client_address)
+        # We use the component manager to store some more global data there.
         mnemosyne = self.server.mnemosyne
-        # Hijack the component manager to store some more global data there.
         mnemosyne.component_manager.socket = socket
-        mnemosyne.component_manager.client_address = self.client_address       
-
-        exec(data)
+        mnemosyne.component_manager.client_address = self.client_address
+        if data != "exit()":
+            try:
+                exec(data)
+            except:
+                socket.sendto("__EXCEPTION__\n" + traceback_string(),
+                    self.client_address)
+        else:
+            self.server.stopped = True
+        socket.sendto("__DONE__\n", self.client_address)
 
 
 class Server(SocketServer.UDPServer):
 
     def __init__(self, port):
-
         self.mnemosyne = Mnemosyne()
         self.mnemosyne.components.insert(0,
             ("mnemosyne.libmnemosyne.translator", "GetTextTranslator"))
         self.mnemosyne.components.append(\
-            ("mnemosyne.UDP_server.UDP_main_window",
-             "UDP_MainWindow"))
+            ("mnemosyne.UDP_server.UDP_main_widget",
+             "UDP_MainWidget"))
         self.mnemosyne.components.append(\
             ("mnemosyne.UDP_server.UDP_review_widget",
-             "UDP_ReviewWidget"))
-        
+             "UDP_ReviewWidget"))    
         SocketServer.UDPServer.__init__(self, ("localhost", port), MyHandler)
-
         print "Server listening on port", port
         self.stopped = False
         while not self.stopped:
-            # We time out every 0.25 seconds, so that we changing
+            # We time out every 0.25 seconds, so that changing
             # self.stopped can have an effect.
             if select.select([self.socket], [], [], 0.25)[0]:
                 self.handle_request()
@@ -49,7 +65,8 @@ class Server(SocketServer.UDPServer):
 
 
 if __name__ == "__main__":
-    server = Server(6666)
+    port = int(sys.argv[1])
+    server = Server(port)
 
 
 
